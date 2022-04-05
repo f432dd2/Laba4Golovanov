091206efa1ce61efd4a0646942e4ca3538874217
Illidan <Illidan@DESKTOP-T8A0CQH>
2022-04-05 13:27:17 +0300

    Изменение

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace Лаб4
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
